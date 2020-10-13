ee51311f545960e97e731f5b4dad40d39a4e6464
Yury Mikushov <mikushov.yu@gmail.com>
2020-10-13 22:06:37 +0300

    Implemented stack

---
+++ stack.js
@@ -0,0 +1,48 @@
+class Stack {
+    constructor() {
+        this.storage = [];
+        this.length = 0;
+    }
+
+    add(value) {
+        this.storage.push(value);
+        this.length ++;
+    }
+
+    delete() {
+        if(this.isEmpty()) {
+            return null;
+        } else {
+            this.length --;
+            return this.storage.pop();
+        }
+    }
+
+    isEmpty() {
+        return this.length == 0; 
+    }
+
+    peek() {
+        if(this.isEmpty()) {
+            return null;
+        } else {
+            return this.storage[this.length - 1];
+        }
+    }
+}
+
+MyStack = new Stack()
+
+MyStack.isEmpty(); // true
+
+MyStack.peek(); // null
+
+MyStack.add(1); // [1]
+MyStack.add(2); // [1, 2]
+MyStack.add(3); // [1, 2, 3]
+
+MyStack.delete(); // 3
+
+MyStack.isEmpty(); // false
+
+MyStack.peek(); // 2
