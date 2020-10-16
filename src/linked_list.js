9e0507eb6be1d3076c98df7dcb0e4f458b0671f6
Yury Mikushov <mikushov.yu@gmail.com>
2020-10-16 22:28:26 +0300

    implemented the class structure

---
+++ src/linked_list.js
@@ -0,0 +1,40 @@
+class LinkedList {
+    constructor() {
+        this._storage = {};
+        this._length = 0;
+    }
+
+    add(value) {
+        
+    }
+
+    addAtEnd(value) {
+        
+    }
+
+    addAtStart(value) {
+
+    }
+
+    delete() {
+
+    }
+
+    deleteAtEnd() {
+        
+    }
+
+    deleteAtStart() {
+
+    }
+
+    search() {
+
+    }
+
+    isEmpty() {
+        return this._length == 0;
+    }
+}
+
+module.exports = Queue;
