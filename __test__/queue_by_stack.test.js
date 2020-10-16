32a12e8547c83f724ce349355ffe43c7d0e4a9ac
Yury Mikushov <mikushov.yu@gmail.com>
2020-10-16 21:32:04 +0300

    added tests for queue by stack

---
+++ __test__/queue_by_stack.test.js
@@ -0,0 +1,43 @@
+const Queue = require("./../src/queue_by_stack.js");
+
+test('new queue is empty', () => {
+    expect((new Queue()).isEmpty()).toBe(true);
+});
+
+test('(empty queue) peek returns null', () => {
+    expect((new Queue()).peek()).toBe(null);
+});
+
+test('(empty queue) delete returns null', () => {
+    expect((new Queue()).delete()).toBe(null);
+});
+
+test('(filled queue) peek returns first element', () => {
+    let queue = new Queue();
+
+    queue.add(1);
+    queue.add(2);
+    queue.add(3);
+
+    expect(queue.peek()).toBe(1);
+});
+
+test('(filled queue) delete returns first element', () => {
+    let queue = new Queue();
+
+    queue.add(1);
+    queue.add(2);
+    queue.add(3);
+
+    expect(queue.delete()).toBe(1);
+});
+
+test('filled queue is not empty', () => {
+    let queue = new Queue();
+
+    queue.add(1);
+    queue.add(2);
+    queue.add(3);
+
+    expect(queue.isEmpty()).toBe(false);
+});
