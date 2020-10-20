7f5dd7bad48f8a121f745e7256aa660cd3839d7a
Yury Mikushov <mikushov.yu@gmail.com>
2020-10-20 22:01:04 +0300

    refactored tests for queue

--- __test__/queue.test.js
+++ __test__/queue.test.js
@@ -1,43 +1,63 @@
 const Queue = require("./../src/queue.js");
 
-test('new queue is empty', () => {
-    expect((new Queue()).isEmpty()).toBe(true);
+describe('check insert() method', () => {
+    test('stack contains three elements', () => {
+        let queue = new Queue();
+    
+        queue.insert(1);
+        queue.insert(2);
+        queue.insert(3);
+        
+        expect(queue._storage[0]).toBe(1);
+        expect(queue._storage[1]).toBe(2);
+        expect(queue._storage[2]).toBe(3);
+    });
 });
 
-test('(empty queue) peek returns undefined', () => {
-    expect((new Queue()).peek()).toBe(undefined);
+describe('check isEmpty() method', () => {
+    test('new queue is empty', () => {
+        expect((new Queue()).isEmpty()).toBe(true);
+    });
+
+    test('filled queue is not empty', () => {
+        let queue = new Queue();
+    
+        queue.insert(1);
+        queue.insert(2);
+        queue.insert(3);
+    
+        expect(queue.isEmpty()).toBe(false);
+    });
 });
 
-test('(empty queue) delete returns undefined', () => {
-    expect((new Queue()).delete()).toBe(undefined);
+describe('check peek() method', () => {
+    test('(empty queue) peek returns undefined', () => {
+        expect((new Queue()).peek()).toBe(undefined);
+    });
+
+    test('(filled queue) peek returns first element', () => {
+        let queue = new Queue();
+    
+        queue.insert(1);
+        queue.insert(2);
+        queue.insert(3);
+    
+        expect(queue.peek()).toBe(1);
+    });
 });
 
-test('(filled queue) peek returns first element', () => {
-    let queue = new Queue();
-
-    queue.insert(1);
-    queue.insert(2);
-    queue.insert(3);
-
-    expect(queue.peek()).toBe(1);
-});
-
-test('(filled queue) delete returns first element', () => {
-    let queue = new Queue();
-
-    queue.insert(1);
-    queue.insert(2);
-    queue.insert(3);
-
-    expect(queue.delete()).toBe(1);
-});
-
-test('filled queue is not empty', () => {
-    let queue = new Queue();
-
-    queue.insert(1);
-    queue.insert(2);
-    queue.insert(3);
-
-    expect(queue.isEmpty()).toBe(false);
+describe('check delete() method', () => {
+    test('(empty queue) delete returns undefined', () => {
+        expect((new Queue()).delete()).toBe(undefined);
+    });
+    
+    test('(filled queue) delete returns first element', () => {
+        let queue = new Queue();
+    
+        queue.insert(1);
+        queue.insert(2);
+        queue.insert(3);
+    
+        expect(queue.delete()).toBe(1);
+    });
 });
