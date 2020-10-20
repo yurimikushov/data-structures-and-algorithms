3de0241d459a7126b0396a113944d736e11103c3
Yury Mikushov <mikushov.yu@gmail.com>
2020-10-20 21:21:08 +0300

    refactored tests for stack

--- __test__/stack.test.js
+++ __test__/stack.test.js
@@ -1,43 +1,50 @@
 const Stack = require("./../src/stack.js");
 
-test('new stack is empty', () => {
-    expect((new Stack()).isEmpty()).toBe(true);
+describe('check isEmpty() method', () => {
+    test('new stack is empty', () => {
+        expect((new Stack()).isEmpty()).toBe(true);
+    });
+
+    test('filled stack is not empty', () => {
+        let stack = new Stack();
+    
+        stack.insert(1);
+        stack.insert(2);
+        stack.insert(3);
+    
+        expect(stack.isEmpty()).toBe(false);
+    });
 });
 
-test('(empty stack) peek returns null', () => {
-    expect((new Stack()).peek()).toBe(null);
+describe('check peek() method', () => {
+    test('(empty stack) peek returns null', () => {
+        expect((new Stack()).peek()).toBe(null);
+    });
+
+    test('(filled stack) peek returns last element', () => {
+        let stack = new Stack();
+    
+        stack.insert(1);
+        stack.insert(2);
+        stack.insert(3);
+    
+        expect(stack.peek()).toBe(3);
+    });
 });
 
-test('(empty stack) delete returns null', () => {
-    expect((new Stack()).delete()).toBe(null);
-});
-
-test('(filled stack) peek returns last element', () => {
-    let stack = new Stack();
-
-    stack.insert(1);
-    stack.insert(2);
-    stack.insert(3);
-
-    expect(stack.peek()).toBe(3);
-});
-
-test('(filled stack) delete returns last element', () => {
-    let stack = new Stack();
-
-    stack.insert(1);
-    stack.insert(2);
-    stack.insert(3);
-
-    expect(stack.delete()).toBe(3);
-});
-
-test('filled stack is not empty', () => {
-    let stack = new Stack();
-
-    stack.insert(1);
-    stack.insert(2);
-    stack.insert(3);
-
-    expect(stack.isEmpty()).toBe(false);
+describe('check delete() method', () => {
+    test('(empty stack) delete returns null', () => {
+        expect((new Stack()).delete()).toBe(null);
+    });
+    
+    
+    test('(filled stack) delete returns last element', () => {
+        let stack = new Stack();
+    
+        stack.insert(1);
+        stack.insert(2);
+        stack.insert(3);
+    
+        expect(stack.delete()).toBe(3);
+    });    
 });
