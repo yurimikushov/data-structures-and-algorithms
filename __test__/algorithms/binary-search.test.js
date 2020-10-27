ae6db4930f951f88205b9e3334c4fd93280245d7
Yury Mikushov <mikushov.yu@gmail.com>
2020-10-27 21:44:07 +0300

    moved binary search method from the separate algorithms repository

---
+++ __test__/algorithms/binary-search.test.js
@@ -0,0 +1,26 @@
+const binarySearch = require('./../../src/algorithms/binary-search.js');
+
+describe('check binary search method', () => {
+    test('find in number array', () => {
+        expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1)).toBe(0);
+        expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3)).toBe(2);
+        expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7)).toBe(6);
+        expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10)).toBe(9);
+    });
+
+    test('find in word array', () => {
+        expect(binarySearch(['Anna', 'Ben', 'Dan', 'Ella', 'Freda'], 'Anna')).toBe(0);
+        expect(binarySearch(['Anna', 'Ben', 'Dan', 'Ella', 'Freda'], 'Dan')).toBe(2);
+        expect(binarySearch(['Anna', 'Ben', 'Dan', 'Ella', 'Freda'], 'Freda')).toBe(4);
+    });
+
+    test('dont find in number array', () => {
+        expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0)).toBe(null);
+        expect(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11)).toBe(null);
+    });
+
+    test('dont find in word array', () => {
+        expect(binarySearch(['Anna', 'Ben', 'Dan', 'Ella', 'Freda'], 'Bob')).toBe(null);
+        expect(binarySearch(['Anna', 'Ben', 'Dan', 'Ella', 'Freda'], 'Mike')).toBe(null);
+    });
+});
---
+++ src/algorithms/binary-search.js
@@ -0,0 +1,24 @@
+function binarySearch(arr, value) {
+    let start = 0;
+    let end = arr.length - 1;
+
+    while (start <= end) {
+        let middle = Math.round((start + end) / 2);
+
+        let guess = arr[middle];
+
+        if (guess == value) {
+            return middle;
+        }
+
+        if (guess < value) {
+            start = middle + 1;
+        } else {
+            end = middle - 1;
+        }
+    }
+
+    return null;
+}
+
+module.exports = binarySearch;
