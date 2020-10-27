a10b31ed05b65abd96f2a38d556717396030ce8f
Yury Mikushov <mikushov.yu@gmail.com>
2020-10-27 21:46:23 +0300

    moved selection sort method from the separate algorithms repository

---
+++ src/algorithms/selection-sort.js
@@ -0,0 +1,26 @@
+function selectionSort(arr) {
+    const smallest = arr => {
+        let smallestId = 0,
+            smallestValue = arr[0];
+
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] < smallestValue) {
+                smallestId = i;
+                smallestValue = arr[i];
+            }
+        }
+
+        return smallestId;
+    };
+
+    let result = [];
+
+    while (arr.length) {
+        result.push(arr[smallest(arr)]);
+        arr.splice(smallest(arr), 1);
+    }
+
+    return result;
+}
+
+module.exports = selectionSort;
