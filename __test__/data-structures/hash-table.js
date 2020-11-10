034dd34f1bc4349ce0da4a444e57a96e35c00bfa
Yuri Mikushov <mikushov.yu@gmail.com>
2020-11-10 19:46:03 +0300

    added the basis of the hash table

---
+++ __test__/data-structures/hash-table.js
@@ -0,0 +1,7 @@
+const HashTable = require('../../src/data-structures/hash-table.js')
+
+describe('check insert() method', () => {})
+
+describe('check get() method', () => {})
+
+describe('check remove() method', () => {})
---
+++ src/data-structures/hash-table.js
@@ -0,0 +1,13 @@
+class HashTable {
+  constructor() {
+    this._storage = []
+  }
+
+  insert(key, value) {}
+
+  get(key) {}
+
+  remove(key) {}
+}
+
+module.exports = HashTable
