e401cd037b681718ae845598feb7956598df3386
Yuri Mikushov <mikushov.yu@gmail.com>
2020-11-12 18:50:26 +0300

    implemented insert() method

--- __test__/data-structures/hash-table.test.js
+++ __test__/data-structures/hash-table.test.js
@@ -22,7 +22,68 @@ describe('check _hash() method', () => {
   })
 })
 
-describe('check insert() method', () => {})
+describe('check insert() method', () => {
+  const item = (hashTable, key) => hashTable._storage[hashTable._hash(key)]
+
+  test('insert a new key-value', () => {
+    let hashTable = new HashTable()
+
+    hashTable.insert('key1', 1)
+    hashTable.insert('key2', 2)
+    hashTable.insert('key3', 3)
+
+    expect(item(hashTable, 'key1')).toEqual({
+      key: 'key1',
+      value: 1,
+      next: undefined,
+    })
+    expect(item(hashTable, 'key2')).toEqual({
+      key: 'key2',
+      value: 2,
+      next: undefined,
+    })
+    expect(item(hashTable, 'key3')).toEqual({
+      key: 'key3',
+      value: 3,
+      next: undefined,
+    })
+  })
+
+  test('insert a exists key', () => {
+    let hashTable = new HashTable()
+
+    hashTable.insert('key1', 1)
+    hashTable.insert('key1', 2)
+
+    expect(item(hashTable, 'key1')).toEqual({
+      key: 'key1',
+      value: 2,
+      next: undefined,
+    })
+  })
+
+  test('insert a exists key', () => {
+    let hashTable = new HashTable()
+
+    hashTable.insert('key1', 1)
+    hashTable.insert('key12', 2)
+    hashTable.insert('key113', 3)
+
+    expect(item(hashTable, 'key1')).toEqual({
+      key: 'key113',
+      value: 3,
+      next: {
+        key: 'key12',
+        value: 2,
+        next: {
+          key: 'key1',
+          value: 1,
+          next: undefined,
+        },
+      },
+    })
+  })
+})
 
 describe('check get() method', () => {})
 
--- src/data-structures/hash-table.js
+++ src/data-structures/hash-table.js
@@ -1,10 +1,33 @@
 class HashTable {
   constructor(size = 10) {
-    this._storage = []
+    this._storage = new Array(size)
     this._size = size
   }
 
-  insert(key, value) {}
+  insert(key, value) {
+    let isKeyExists = false
+
+    let item = this._storage[this._hash(key)]
+
+    while (item) {
+      if (item.key == key) {
+        item.key = key
+        item.value = value
+        isKeyExists = true
+        break
+      }
+
+      item = item.next
+    }
+
+    if (!isKeyExists) {
+      this._storage[this._hash(key)] = {
+        key: key,
+        value: value,
+        next: this._storage[this._hash(key)],
+      }
+    }
+  }
 
   get(key) {}
 
