af12e0333ef6bf346f74e2cee8867134f4d439fa
Yuri Mikushov <mikushov.yu@gmail.com>
2020-11-14 21:04:11 +0300

    refoctored linked list tests

--- __test__/data-structures/linked-list.test.js
+++ __test__/data-structures/linked-list.test.js
@@ -4,21 +4,34 @@ describe('check isEmpty() method', () => {
   test('new linked list is empty', () => {
     expect(new LinkedList().isEmpty()).toBe(true)
   })
+
+  test('filled linked list is not empty', () => {
+    let linkedList = new LinkedList()
+
+    linkedList.insertAtEnd(1)
+
+    expect(linkedList.isEmpty()).toBe(false)
+  })
 })
 
 describe('check insertAtEnd() method', () => {
-  test('insert three elements', () => {
+  test('insert three elements to end', () => {
     let linkedList = new LinkedList()
 
     linkedList.insertAtEnd(1)
     linkedList.insertAtEnd(2)
     linkedList.insertAtEnd(3)
 
-    let storage = linkedList._storage
-
-    expect(storage.value).toBe(1)
-    expect(storage.next.value).toBe(2)
-    expect(storage.next.next.value).toBe(3)
+    expect(linkedList._storage).toEqual({
+      value: 1,
+      next: {
+        value: 2,
+        next: {
+          value: 3,
+          next: undefined,
+        },
+      },
+    })
   })
 })
 
@@ -32,38 +45,46 @@ describe('check insertAtStart() method', () => {
 
     linkedList.insertAtStart(4)
 
-    let storage = linkedList._storage
-
-    expect(storage.value).toBe(4)
-    expect(storage.next.value).toBe(1)
-    expect(storage.next.next.value).toBe(2)
-    expect(storage.next.next.next.value).toBe(3)
+    expect(linkedList._storage).toEqual({
+      value: 4,
+      next: {
+        value: 1,
+        next: {
+          value: 2,
+          next: {
+            value: 3,
+            next: undefined,
+          },
+        },
+      },
+    })
   })
 })
 
 describe('check insertAfter() method', () => {
-  test('insert one element', () => {
+  test('insert one element to middle', () => {
     let linkedList = new LinkedList()
 
     linkedList.insertAtEnd(1)
     linkedList.insertAtEnd(2)
-    linkedList.insertAtEnd(3)
-    linkedList.insertAtEnd(4)
 
-    linkedList.insertAfter(2, 5)
+    linkedList.insertAfter(1, 3)
 
-    let storage = linkedList._storage
-
-    expect(storage.value).toBe(1)
-    expect(storage.next.value).toBe(2)
-    expect(storage.next.next.value).toBe(5)
-    expect(storage.next.next.next.value).toBe(3)
-    expect(storage.next.next.next.next.value).toBe(4)
+    expect(linkedList._storage).toEqual({
+      value: 1,
+      next: {
+        value: 3,
+        next: {
+          value: 2,
+          next: undefined,
+        },
+      },
+    })
   })
 })
 
 describe('check delete() method', () => {
-  test('delete two elements', () => {
+  test('delete two elements at middle', () => {
     let linkedList = new LinkedList()
 
     linkedList.insertAtEnd(1)
@@ -71,18 +92,21 @@ describe('check delete() method', () => {
     linkedList.insertAtEnd(3)
     linkedList.insertAtEnd(4)
 
-    linkedList.delete(1)
     linkedList.delete(2)
+    linkedList.delete(3)
 
-    let storage = linkedList._storage
-
-    expect(storage.value).toBe(3)
-    expect(storage.next.value).toBe(4)
+    expect(linkedList._storage).toEqual({
+      value: 1,
+      next: {
+        value: 4,
+        next: undefined,
+      },
+    })
   })
 })
 
 describe('check deleteAtEnd() method', () => {
-  test('delete from emplty linked list', () => {
+  test('delete from empty linked list', () => {
     expect(new LinkedList().deleteAtStart()).toBe(undefined)
   })
 
@@ -105,7 +129,7 @@ describe('check deleteAtEnd() method', () => {
 })
 
 describe('check deleteAtStart() method', () => {
-  test('delete from emplty linked list', () => {
+  test('delete from empty linked list', () => {
     expect(new LinkedList().deleteAtStart()).toBe(undefined)
   })
 
@@ -117,21 +141,26 @@ describe('check deleteAtStart() method', () => {
     linkedList.insertAtEnd(3)
 
     expect(linkedList.deleteAtStart()).toBe(1)
-
-    expect(linkedList._storage.value).toBe(2)
-    expect(linkedList._storage.next.value).toBe(3)
+    expect(linkedList._storage).toEqual({
+      value: 2,
+      next: {
+        value: 3,
+        next: undefined,
+      },
+    })
   })
 })
 
 describe('check isContain() method', () => {
-  test('check two elements', () => {
+  test('linked list is contain checked element', () => {
     let linkedList = new LinkedList()
 
     linkedList.insertAtEnd(1)
-    linkedList.insertAtEnd(2)
-    linkedList.insertAtEnd(3)
 
-    expect(linkedList.isContain(2)).toBe(true)
-    expect(linkedList.isContain(4)).toBe(false)
+    expect(linkedList.isContain(1)).toBe(true)
+  })
+
+  test('linked list is not contain checked element', () => {
+    expect(new LinkedList().isContain(1)).toBe(false)
   })
 })
